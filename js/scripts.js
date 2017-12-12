a2ad49841427f4bd02d4d6b199ad364b52257014
Siddharth Kundu <sk31395@gmail.com>
2017-12-13 02:26:54 +0530

    Complete website

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+
+//Hamburger menu toggle
+  $('.nav-toggle').click(function() {
+    $('.main-nav').toggleClass('is-open'); //Adding is-open class to main-nav class
+    $('.hamburger').toggleClass('is-open');    
+  })
+
+})
